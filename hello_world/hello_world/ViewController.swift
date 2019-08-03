255f8c6d518e66d5bd28abe3fdba80deb7f6efb2
Space Shuttle <spaceshuttle@Space-Shuttle.local>
2019-08-02 20:39:39 -0700

    edited swift project

--- hello_world/hello_world/ViewController.swift
+++ hello_world/hello_world/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var firstNumberOutlet: UITextField!
+    
+    @IBOutlet weak var secondNumberOutlet: UITextField!
+    
+    @IBAction func addButtonAction(_ sender: Any) {
+        let int1 = Int(firstNumberOutlet.text!)
+        let int2 = Int(secondNumberOutlet.text!)
+        labelOutlet.text = String(int1! + int2!)
+    }
+    
+    @IBAction func subtractButtonAction(_ sender: Any) {
+        let int1 = Int(firstNumberOutlet.text!)
+        let int2 = Int(secondNumberOutlet.text!)
+        labelOutlet.text = String(int1! - int2!)
+    }
+    
+    @IBAction func multiplyButtonAction(_ sender: Any) {
+        let int1 = Int(firstNumberOutlet.text!)
+        let int2 = Int(secondNumberOutlet.text!)
+        labelOutlet.text = String(int1! * int2!)
+    }
+    
+    @IBAction func divideButtonAction(_ sender: Any) {
+        let int1 = Int(firstNumberOutlet.text!)
+        let int2 = Int(secondNumberOutlet.text!)
+        labelOutlet.text = String(int1! / int2!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
