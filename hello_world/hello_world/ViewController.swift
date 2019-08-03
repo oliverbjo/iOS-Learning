2b912e27b8985b9f020288de2f92cd9d4d632e5d
Space Shuttle <spaceshuttle@Space-Shuttle.local>
2019-08-02 19:42:05 -0700

    created swift project

---
+++ hello_world/hello_world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Space Shuttle on 8/2/19.
+//  Copyright © 2019 Oliver Bjorkman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hello_world/hello_worldTests/hello_worldTests.swift
@@ -0,0 +1,34 @@
+//
+//  hello_worldTests.swift
+//  hello_worldTests
+//
+//  Created by Space Shuttle on 8/2/19.
+//  Copyright © 2019 Oliver Bjorkman. All rights reserved.
+//
+
+import XCTest
+@testable import hello_world
+
+class hello_worldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
