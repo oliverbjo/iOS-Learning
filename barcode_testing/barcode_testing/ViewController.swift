9f1ad1821ddf70770310e147d5d2b527653c475a
Space Shuttle <spaceshuttle@Space-Shuttle.local>
2019-08-04 19:30:45 -0700

    created project

---
+++ barcode_testing/barcode_testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  barcode_testing
+//
+//  Created by Space Shuttle on 8/4/19.
+//  Copyright © 2019 Oliver Bjorkman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
