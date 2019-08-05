050a2d3aaf64ee65b64842eea2051fa356c4fab1
Space Shuttle <spaceshuttle@Space-Shuttle.local>
2019-08-04 20:45:55 -0700

    edited project

--- barcode_testing/barcode_testing/ViewController.swift
+++ barcode_testing/barcode_testing/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var a: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let stored = UserDefaults.standard.string(forKey: "a")
+
+        if stored != nil {
+            barcodeOutlet.image = UIImage(barcode: stored!)
+        }
     }
+    
+    @IBOutlet weak var barcodeOutlet: UIImageView!
 
+    @IBOutlet weak var errorLabelOutlet: UILabel!
+    
+    
+    
+    @IBOutlet weak var studentIDOutleta: UITextField!
+    
+    @IBAction func generateBarcodeAction(_ sender: Any) {
+        
+        a = studentIDOutleta.text
+        if a == "" {
+            errorLabelOutlet.text = "Please enter your student ID Number"
+        }
+        
+        let barcode = UIImage(barcode: studentIDOutleta.text!)
+        barcodeOutlet.image = barcode
+        
+    }
+    
+    @IBAction func shareImageAction(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        UserDefaults.standard.set(a, forKey: "a")
+        
+    }
+    
+}
 
+extension UIImage {
+    
+    convenience init?(barcode: String) {
+        let data = barcode.data(using: .ascii)
+        guard let filter = CIFilter(name: "CICode128BarcodeGenerator") else {
+            return nil
+        }
+        filter.setValue(data, forKey: "inputMessage")
+        guard let ciImage = filter.outputImage else {
+            return nil
+        }
+        self.init(ciImage: ciImage)
+    }
+    
 }
 
